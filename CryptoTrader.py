47a9602cdb0c4a8cf4ccea229faf337ae9ffbe22
Simon Lee <simonlee@me.com>
2021-03-19 08:48:30 +0800

    Updated: 2021-03-19 08:48 AM

--- CryptoTrader.py
+++ CryptoTrader.py
@@ -9,7 +9,7 @@ import time
 ########
 TRADE_BTC_NOTIONAL = 10000
 TRADE_ETH_NOTIONAL = 5000
-TRADE_FTT_NOTIONAL = 1000
+TRADE_FTT_NOTIONAL = 2000
 
 ########
 # Limits
@@ -133,7 +133,7 @@ print('Target FTT: '+str(round(trade_ftt,6))+' ($'+str(round(trade_ftt_notional)
 ######
 if False:
   ##########################
-  ccy = 'ETH'  # <----------
+  ccy = 'FTT'  # <----------
   ##########################
   if ccy=='BTC':
     trade_coin=trade_btc
@@ -147,16 +147,16 @@ if False:
   else:
     sl.stop()
 
-  ftxRelOrder('BUY', ftx, ccy+'/USD', trade_coin) # FTX Spot Buy (Maker)
-  #ftxRelOrder('SELL', ftx, ccy+'/USD, trade_coin) # FTX Spot Sell (Maker)
+  #ftxRelOrder('BUY', ftx, ccy+'/USD', trade_coin) # FTX Spot Buy (Maker)
+  ftxRelOrder('SELL', ftx, ccy+'/USD', trade_coin) # FTX Spot Sell (Maker)
 
-  #ftxRelOrder('BUY', ftx, ccy + '-PERP', trade_coin)  # FTX Fut Buy (Maker)
+  ftxRelOrder('BUY', ftx, ccy + '-PERP', trade_coin)  # FTX Fut Buy (Maker)
   #ftxRelOrder('SELL', ftx, ccy+'-PERP', trade_coin) # FTX Fut Sell (Maker)
 
   # bnMarketOrder('BUY', bn, ccy, trade_usd) # Binance Fut Buy (Taker)
   #bnMarketOrder('SELL', bn, ccy, trade_usd)  # Binance Fut Sell (Taker)
 
   #bbRelOrder('BUY', bb, ccy, trade_usd) # Bybit Fut Buy (Maker)
-  bbRelOrder('SELL', bb, ccy, trade_usd) # Bybit Fut Sell (Maker)
+  #bbRelOrder('SELL', bb, ccy, trade_usd) # Bybit Fut Sell (Maker)
 
   print(sl.getCurrentTime()+': Done')
