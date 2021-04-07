6b0cca8d799b8059802bf81bc8f42be8b1a1e4e3
Simon Lee <simonlee@me.com>
2021-04-07 14:15:15 +0800

    Updated: 2021-04-07 02:15 PM

--- KrakenTrader.py
+++ KrakenTrader.py
@@ -1,9 +1,10 @@
 import CryptoLib as cl
 import numpy as np
+import sys
 
 side='BUY'
-trade_btc_notional=5000
-maxChases=888
+trade_btc_notional=5000 # In USD
+isHedgeWithFTX=True
 
 ######
 # Main
@@ -13,4 +14,13 @@ kr=cl.krCCXTInit()
 ftxWallet=cl.ftxGetWallet(ftx)
 spotBTC=ftxWallet.loc['BTC','spot']
 trade_btc = np.min([np.min([trade_btc_notional, cl.CT_MAX_NOTIONAL]) / spotBTC, cl.CT_MAX_BTC])
-fill=cl.krRelOrder(side,kr,'BTC',trade_btc,maxChases=maxChases)
+fill=cl.krRelOrder(side,kr,'BTC',trade_btc,maxChases=888)
+
+if isHedgeWithFTX:
+  if side=='BUY':
+    oppSide='SELL'
+  elif side=='SELL':
+    oppSide='BUY'
+  else:
+    sys.exit(1)
+fill=cl.ftxRelOrder(oppSide,ftx,'BTC',trade_btc,maxChases=888)
