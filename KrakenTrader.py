96415c302516b7665e0b26dfeec1860d4d0ed2d5
Simon Lee <simonlee@me.com>
2021-04-09 18:58:07 +0800

    Updated: 2021-04-09 06:58 PM

--- KrakenTrader.py
+++ KrakenTrader.py
@@ -7,7 +7,8 @@ import sys
 ########
 side='BUY'          # 'BUY', 'SELL'
 targetUSD=3000
-hedgeExchange='bb'  # 'ftxspot', 'bb', 'none'
+hedgeExchange='kf'  # 'ftxspot', 'bb', 'kf', 'none'
+nPrograms=10
 
 ######
 # Init
@@ -20,6 +21,7 @@ else:
   sys.exit(1)
 ftx = cl.ftxCCXTInit()
 bb = cl.bbCCXTInit()
+kf=cl.kfInit()
 kr=cl.krCCXTInit()
 ftxWallet=cl.ftxGetWallet(ftx)
 spotBTC=ftxWallet.loc['BTC','spot']
@@ -29,10 +31,17 @@ trade_btc_notional = trade_btc * spotBTC
 ######
 # Main
 ######
-cl.printHeader('KrakenTrader -- BTC only')
-fill=cl.krRelOrder(side,kr,'BTC',trade_btc,maxChases=888)
-if hedgeExchange=='ftxspot':
-  fill=cl.ftxRelOrder(oppSide,ftx,'BTC/USD',trade_btc,maxChases=888)
-elif hedgeExchange=='bb':
-  fill=cl.bbRelOrder(oppSide, bb, 'BTC', trade_btc_notional, maxChases=888)
+cl.printHeader('KrakenTrader')
+print('Note that this tool only handles BTC ....')
+
+for n in range(nPrograms):
+  cl.printHeader('KrakenTrader Program '+str(n+1))
+  fill=cl.krRelOrder(side,kr,'BTC',trade_btc,maxChases=888)
+  if hedgeExchange=='ftxspot':
+    fill=cl.ftxRelOrder(oppSide,ftx,'BTC/USD',trade_btc,maxChases=888)
+  elif hedgeExchange=='bb':
+    fill=cl.bbRelOrder(oppSide, bb, 'BTC', trade_btc_notional, maxChases=888)
+  elif hedgeExchange=='kf':
+    fill=cl.kfRelOrder(oppSide, kf, 'BTC', trade_btc_notional, maxChases=888)
+
 cl.speak('KrakenTrader has completed')
