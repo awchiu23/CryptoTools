9af96b40755ff08de603056ac6be57a830841f7c
Simon Lee <simonlee@me.com>
2021-05-15 00:00:01 +0800

    Updated: 2021-05-15 12:00 AM

--- FTXLender.py
+++ FTXLender.py
@@ -11,8 +11,8 @@ isManageCoins=True        # Also manage coins in addition to USD?
 coinsList=['BTC','ETH','XRP']
 
 minRate=0.05              # Minimum rate for all loans
-usdLendingRatio=0.5       # Percentage of USD to lend out
-coinLendingRatio=0.5      # Percentage of coins to lend out
+usdLendingRatio=1/3       # Percentage of USD to lend out
+coinLendingRatio=1/3      # Percentage of coins to lend out
 
 ###########
 # Functions
