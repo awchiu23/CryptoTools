05d6443619116ba0bf264cc168cba79e5b221513
Simon Lee <simonlee@me.com>
2021-05-19 00:00:00 +0800

    Updated: 2021-05-19 12:00 AM

--- FTXTrader.py
+++ FTXTrader.py
@@ -4,15 +4,15 @@ from CryptoParams import *
 ########
 # Params
 ########
-ccy='MATIC'
+ccy='SOL'
 notional=2000 # USD
-tgtBps=25
+tgtBps=30
 color='cyan'
 
 CT_CONFIGS_DICT['SPOT_'+ccy+'_OK']=1
 CT_CONFIGS_DICT['FTX_'+ccy+'_OK']=1
 CT_CONFIGS_DICT['SPOT_'+ccy+'_ADJ_BPS']=0
-CT_CONFIGS_DICT['FTX_'+ccy+'_ADJ_BPS']=-20   # Positive = eager to buy; Negative = eager to sell
+CT_CONFIGS_DICT['FTX_'+ccy+'_ADJ_BPS']=30   # Positive = eager to buy; Negative = eager to sell
 
 CT_CONFIGS_DICT['FTX_DISTANCE_TO_BEST_BPS']=-2
 #SHARED_CCY_DICT[ccy] = {'futExch': ['ftx']}
