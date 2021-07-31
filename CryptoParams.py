def25417aa94568d2a344b1f78546ab339b20137
Simon Lee <simonlee@me.com>
2021-08-01 00:00:00 +0800

    Updated: 2021-08-01 12:00 AM

--- CryptoParams.py
+++ CryptoParams.py
@@ -151,8 +151,8 @@ if os.environ.get('USERNAME')=='Simon':
   SHARED_CCY_DICT['SAND'] = {'futExch': ['ftx','bnt']}
   SHARED_CCY_DICT['SLP'] = {'futExch': ['ftx']}
   #####
-  #CR_AG_CCY_DICT['BTC']=9.595 #bbftx
-  #CR_AG_CCY_DICT['ETH']=112 #bbftx
+  CR_AG_CCY_DICT['BTC']=4.148 #kfftx
+  #CR_AG_CCY_DICT['ETH']=43.727 #bbftx
   #CR_AG_CCY_DICT['XRP'] = 159604 #bbftx
   #CR_AG_CCY_DICT['LTC'] = 374 #ftxkf
   #CR_EXT_DELTA_USDT = 249999 #bbftx
