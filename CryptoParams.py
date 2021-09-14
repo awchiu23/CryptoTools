d5b09b2721193c1907943b0e558d8b0fd8c3c50f
Simon Lee <simonlee@me.com>
2021-09-15 00:00:00 +0800

    Updated: 2021-09-15 12:00 AM

--- CryptoParams.py
+++ CryptoParams.py
@@ -125,17 +125,23 @@ if os.environ.get('USERNAME')=='Simon':
   #APOPHIS_CONFIGS_DICT['IS_IP_WHITELIST'] = False
   SHARED_EXCH_DICT=dict({'ftx':1,'bbt':3,'bb':0,'bnt':0,'bn':0,'bnim':0,'db':0,'kf':1})
   CR_QUOTE_CCY_DICT['XRP'] = 4
+  CR_QUOTE_CCY_DICT['LTC'] = 4
   CR_QUOTE_CCY_DICT['AAVE'] = 4
   CR_QUOTE_CCY_DICT['SOL'] = 4
+  CR_QUOTE_CCY_DICT['ADA'] = 4
   CR_QUOTE_CCY_DICT['EOS'] = 4
   CR_AG_CCY_DICT['XRP'] = 0
+  CR_AG_CCY_DICT['LTC'] = 0
   CR_AG_CCY_DICT['AAVE'] = 0
   CR_AG_CCY_DICT['SOL'] = 0
+  CR_AG_CCY_DICT['ADA'] = 0
   CR_AG_CCY_DICT['EOS'] = 0
-  CR_FTX_FLOWS_CCYS.extend(['XRP','AAVE','SOL'])
+  CR_FTX_FLOWS_CCYS.extend(['XRP','LTC','AAVE','SOL'])
   SHARED_CCY_DICT['XRP'] = {'futExch': ['ftx','bbt','bb','kf']}
+  SHARED_CCY_DICT['LTC'] = {'futExch': ['ftx','bbt']}
   SHARED_CCY_DICT['AAVE'] = {'futExch': ['ftx','bbt']}
   SHARED_CCY_DICT['SOL'] = {'futExch': ['ftx','bbt']}
+  SHARED_CCY_DICT['ADA'] = {'futExch': ['ftx','bbt']}
   SHARED_CCY_DICT['EOS'] = {'futExch': ['ftx','bbt']}
   #####
   #CR_AG_CCY_DICT['BTC']=3.9091 #krftx
