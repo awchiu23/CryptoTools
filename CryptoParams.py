8cc41b3556d48952aac56d49e1b3ed13d8c45dbd
Simon Lee <simonlee@me.com>
2021-10-29 13:11:01 +0800

    Updated: 2021-10-29 01:11 PM

--- CryptoParams.py
+++ CryptoParams.py
@@ -149,7 +149,7 @@ if os.environ.get('USERNAME')=='Simon':
   my_FTX_BBT=[]
   my_FTX_BBT_flowless=[]
   my_FTX_KUT=['XRP','DOGE','MATIC','SOL','SUSHI']
-  my_FTX_KUT_flowless=['ADA','ATOM','LUNA','SHIB','VET']
+  my_FTX_KUT_flowless=['ADA','ATOM','LUNA','VET']
   ############################################################################################################
   for ccy in (my_FTX + my_FTX_BBT_KUT + my_FTX_BBT_KUT_flowless + my_FTX_BBT + my_FTX_BBT_flowless + my_FTX_KUT + my_FTX_KUT_flowless): CR_QUOTE_CCY_DICT[ccy] = 4
   for ccy in (my_FTX_BBT_KUT + my_FTX_BBT_KUT_flowless + my_FTX_BBT + my_FTX_BBT_flowless + my_FTX_KUT + my_FTX_KUT_flowless):  CR_AG_CCY_DICT[ccy] = 0
@@ -158,6 +158,9 @@ if os.environ.get('USERNAME')=='Simon':
   for ccy in (my_FTX_BBT_KUT + my_FTX_BBT_KUT_flowless): SHARED_CCY_DICT[ccy] = {'futExch': ['ftx', 'bbt','kut']}
   for ccy in (my_FTX_BBT + my_FTX_BBT_flowless): SHARED_CCY_DICT[ccy] = {'futExch': ['ftx', 'bbt']}
   for ccy in (my_FTX_KUT + my_FTX_KUT_flowless): SHARED_CCY_DICT[ccy] = {'futExch': ['ftx', 'kut']}
+  ############################################################################################################
+  #SHARED_CCY_DICT['SHIB'] = {'futExch': ['ftx', 'kut']}
+
   #SHARED_CCY_DICT['BTC']['futExch'].remove('bbt')
   #SHARED_CCY_DICT['ETH']['futExch'].remove('bbt')
   SHARED_CCY_DICT['FTT']['futExch'].remove('bbt')
